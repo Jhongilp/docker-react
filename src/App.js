6d89caa96bd334548736aaba3ddaf861d5bec037
jhongilp <jhongilp@gmail.com>
2022-04-07 20:54:47 -0500

    change app text to test redeployment on merge

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello!
+          I was changed from feature branch!
         </p>
         <a
           className="App-link"
